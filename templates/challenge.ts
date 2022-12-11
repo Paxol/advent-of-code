4f43f46c6320506c30e7237f79d6028c69802fad
Paxol <mirko.passoli@gmail.com>
2022-12-11 02:00:54 +0100

    Updated template

--- templates/challenge.ts
+++ templates/challenge.ts
@@ -1,14 +1,14 @@
 // Year __year Day __day
 
 async function parseInput (input: string): Promise<void> {
+  // const splittedInput = input.split("\n").filter(s => s.trim() !== "");
+}
+
+async function phaseOne(): Promise<string | number | void> {
   
 }
 
-async function phaseOne(): Promise<string | void> {
-  
-}
-
-async function phaseTwo(): Promise<string | void> {
+async function phaseTwo(): Promise<string | number | void> {
   
 }
 
